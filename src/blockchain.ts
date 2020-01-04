7af44ba1e6387e3a9c1e3688e88f688dfb0e3d5b
alexpatapan <42231868+alexpatapan@users.noreply.github.com>
2020-01-04 20:16:54 +1000

    Add files via upload

--- src/blockchain.ts
+++ src/blockchain.ts
@@ -21,19 +21,31 @@ class Block {
 const calculateHash = (index: number, previousHash: string, timestamp: number, data: string): string =>
     CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
 
+const calculateHashForBlock = (block: Block): string => {
+    return calculateHash(block.index, block.previousHash, block.timestamp, block.data);
+}
+
 
 // Setup the initial block (the only block without a previousHash)
 const genesisBlock: Block = new Block(
     0, '816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7', null, 1465154705, 'my genesis block!!'
 );
 
-
+// Initialisation of blockchain
 let blockchain: Block[] = [genesisBlock];
 
 const getBlockchain = (): Block[] => blockchain;
 
 const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
 
-const generateNextBlock = (blockData : string) => {
-    
+const generateNextBlock = (blockData: string) => {
+    const previousBlock: Block = getLatestBlock();
+    const nextIndex: number = previousBlock.index + 1;
+    const nextTimestamp: number = new Date().getTime() / 1000;
+    const nextHash: string = calculateHash(nextIndex, previousBlock.hash, nextTimestamp, blockData);
+    const newBlock: Block = new Block(nextIndex, nextHash, previousBlock.hash, nextTimestamp, blockData);
+    addBlock(newBlock);
+    broadcastLatest();
+    return newBlock;
+
 }
